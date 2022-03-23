fbfc800b48c9cfc064a18f914777b1a7d560ef3a
Jonathan Guberman <dev@jonathanguberman.com>
2022-03-22 21:25:02 -0400

    Basic text output; bad word list

---
+++ want-what-you-bot.py
@@ -0,0 +1,51 @@
+from collections import defaultdict
+import random
+
+from nltk.corpus import cmudict as cmu, wordnet as wn
+
+class CategorizedWords:
+  def __init__(self, wordlist):
+    self.syllable_map = defaultdict(list)
+    self.nouns = [[], [], []]
+    self.adjectives = []
+
+    for word in wordlist:
+      stress_pattern = syllables(word)
+      self.syllable_map[syllables(word)].append(word)
+
+      if wn.synsets(word, pos=wn.NOUN):
+        if stress_pattern == (1,):
+          self.nouns[0].append(word)
+        elif stress_pattern == (1, 0) or stress_pattern == (1,2):
+          self.nouns[1].append(word)
+        elif stress_pattern == (0, 1, 0):
+          self.nouns[2].append(word)
+      elif wn.synsets(word, pos=wn.ADJ):
+        if stress_pattern == (1,):
+          self.adjectives.append(word)
+
+
+CMUDICT = cmu.dict()
+
+def syllables(word):
+  # return tuple(int(s[-1]) for s in cmudict[word.lower()] if s[-1].isdigit())
+  return tuple(int(s[-1]) for s in CMUDICT[word.lower()][0] if s[-1].isdigit())
+
+def random_lyrics():
+  categorized_words = CategorizedWords(cmu.words())
+  lyrics = f'''
+  I want your {random.choice(categorized_words.nouns[1])}
+  I want your {random.choice(categorized_words.nouns[1])}
+  I want what you got, I say it a lot
+  I want your {random.choice(categorized_words.adjectives)} {random.choice(categorized_words.nouns[0])}
+  Want your {random.choice(categorized_words.nouns[2])}
+  I want what you got, I say it a lot
+  Oh my god, oh my god, oh my god, oh my god
+  I want what you got, I say it a lot
+  I want your {random.choice(categorized_words.nouns[0])}, I
+  Want your {random.choice(categorized_words.nouns[2])}
+  I want what you got, I say it a lot
+  '''
+  return lyrics
+
+print(random_lyrics())
