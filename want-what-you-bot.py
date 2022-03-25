5811d6feb19bf79f26e6d6e1e291561f64d07f04
Jonathan Guberman <dev@jonathanguberman.com>
2022-03-24 21:35:55 -0400

    Code improvements and XML test generation

--- want-what-you-bot.py
+++ want-what-you-bot.py
@@ -1,34 +1,76 @@
+from collections import defaultdict
+from enum import Enum, auto
 import random
+from syllabifications import get_syllables
 
-class CategorizedWords:
-  def __init__(self, nouns, adjectives):
-    self.nouns = nouns
-    self.adjectives = adjectives
+class PartsOfSpeech(Enum):
+  NOUN = auto()
+  ADJECTIVE = auto()
 
-def random_lyrics(categorized_words):
+class WordChoices:
+  def __init__(self, wordlists):
+      self.mononoun1, self.mononoun2 = random.sample(wordlists[(PartsOfSpeech.NOUN, 1)], 2)
+      self.trocheenoun1, self.trocheenoun2 = random.sample(wordlists[(PartsOfSpeech.NOUN, 2)], 2)
+      self.amphinoun1, self.amphinoun2 = random.sample(wordlists[(PartsOfSpeech.NOUN, 3)], 2)
+      self.adj = random.choice(wordlists[(PartsOfSpeech.ADJECTIVE, 1)])
+
+
+def random_lyrics(words):
   lyrics = f'''
-  I want your {random.choice(categorized_words.nouns[1])}
-  I want your {random.choice(categorized_words.nouns[1])}
+  I want your {words.trocheenoun1}
+  I want your {words.trocheenoun2}
   I want what you got, I say it a lot
-  I want your {random.choice(categorized_words.adjectives)} {random.choice(categorized_words.nouns[0])}
-  Want your {random.choice(categorized_words.nouns[2])}
+  I want your {words.adj} {words.mononoun1}
+  Want your {words.amphinoun1}
   I want what you got, I say it a lot
   Oh my god, oh my god, oh my god, oh my god
   I want what you got, I say it a lot
-  I want your {random.choice(categorized_words.nouns[0])}, I
-  Want your {random.choice(categorized_words.nouns[2])}
+  I want your {words.mononoun2}, I
+  Want your {words.amphinoun2}
   I want what you got, I say it a lot
   '''
   return lyrics
+
+def singing_xml(words):
+  xml = f'''
+  <!DOCTYPE SINGING PUBLIC "-//SINGING//DTD SINGING mark up//EN"
+      "Singing.v0_1.dtd">
+<SINGING BPM="112">
+<PITCH NOTE="A4"><DURATION BEATS="1.0">{get_syllables(words.trocheenoun1)[0]}</DURATION></PITCH>
+<PITCH NOTE="A4"><DURATION BEATS="1.0">{get_syllables(words.trocheenoun1)[1]}</DURATION></PITCH>
+<REST BEATS="1.0"></REST>
+<PITCH NOTE="A4"><DURATION BEATS="1.0">{get_syllables(words.trocheenoun2)[0]}</DURATION></PITCH>
+<PITCH NOTE="A4"><DURATION BEATS="1.0">{get_syllables(words.trocheenoun2)[1]}</DURATION></PITCH>
+<REST BEATS="1.0"></REST>
+<PITCH NOTE="A4"><DURATION BEATS="1.0">{words.adj}</DURATION></PITCH>
+<PITCH NOTE="A4"><DURATION BEATS="1.0">{words.mononoun1}</DURATION></PITCH>
+<REST BEATS="1.0"></REST>
+<PITCH NOTE="A4"><DURATION BEATS="0.5">{get_syllables(words.amphinoun1)[0]}</DURATION></PITCH>
+<PITCH NOTE="A4"><DURATION BEATS="1.0">{get_syllables(words.amphinoun1)[1]}</DURATION></PITCH>
+<PITCH NOTE="A4"><DURATION BEATS="0.5">{get_syllables(words.amphinoun1)[2]}</DURATION></PITCH>
+<REST BEATS="1.0"></REST>
+<PITCH NOTE="A4"><DURATION BEATS="1.0">{words.mononoun2}</DURATION></PITCH>
+<REST BEATS="1.0"></REST>
+<PITCH NOTE="A4"><DURATION BEATS="0.5">{get_syllables(words.amphinoun2)[0]}</DURATION></PITCH>
+<PITCH NOTE="A4"><DURATION BEATS="1.0">{get_syllables(words.amphinoun2)[1]}</DURATION></PITCH>
+<PITCH NOTE="A4"><DURATION BEATS="0.5">{get_syllables(words.amphinoun2)[2]}</DURATION></PITCH>
+</SINGING>
+  '''
+  return xml
 
 def file_to_list(filename):
   with open(filename) as fp:
     return [line.strip() for line in fp]
 
 if __name__ == "__main__":
-  ones = file_to_list("./wordlists/NN-1.txt")
-  twos = file_to_list("./wordlists/NN-2.txt")
-  threes = file_to_list("./wordlists/NN-3.txt")
-  adj = file_to_list("./wordlists/JJ-1.txt")
+  wordlists = defaultdict(list, {
+    (PartsOfSpeech.NOUN, 1): file_to_list("./wordlists/NN-1.txt"),
+    (PartsOfSpeech.NOUN, 2): file_to_list("./wordlists/NN-2.txt"),
+    (PartsOfSpeech.NOUN, 3): file_to_list("./wordlists/NN-3.txt"),
+    (PartsOfSpeech.ADJECTIVE, 1): file_to_list("./wordlists/JJ-1.txt"),
+  } )
 
-  print(random_lyrics(CategorizedWords([ones, twos, threes], adj)))
+  word_choices = WordChoices(wordlists)
+  print(random_lyrics(word_choices))
+  print()
+  print(singing_xml(word_choices))
